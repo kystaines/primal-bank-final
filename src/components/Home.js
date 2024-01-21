d5faf3f785c69cf94ad2a666b90ac7c90f5ca294
Keziah Ysabel Sta. Ines <144651777+kystaines@users.noreply.github.com>
2024-01-22 00:51:07 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,54 +10,6 @@ import fstMny from "../images/fast_money.png";
 import easyP from "../images/easy_money.png";
 
 const Home = () => {
- Martin
-
-    useEffect(() => {
-        AOS.init({
-            duration: 1000, // Set the animation duration to 1200ms (1.2 seconds)
-        });
-    }, []);
-
-
-    return (
-        <section className="home_cls">
-
-            <div className="home-cntr">
-
-                <div className="home_txt">
-
-                    <p1 className="greetings">Hello! Welcome to,</p1>
-                    <div data-aos="fade-right" data-aos-delay="0" className="name_logo_bx">
-                        <img src={banklogo} alt="" />
-                    </div>
-                    <p>
-                        "Let us help you invest in your future. It's our pleasure to work with you."
-                    </p>
-                    <h3>The official website of Primal Bank</h3>
-
-                    <div className="features">
-                        <div data-aos="fade-up-right" data-aos-delay="20" className="ft">
-                            <img src={easyP} alt="wallet_img" />
-                            <span>Easy to Use</span>
-                        </div>
-                        <div data-aos="fade-up-right" data-aos-delay="20" className="ft">
-                            <img src={fstMny} alt="wallet_img" />
-                            <span>Fast Transactions</span>
-                        </div>
-                        <div data-aos="fade-up-right" data-aos-delay="20" className="ft">
-                            <img src={wallet} alt="wallet_img" />
-                            <span>Secured Payments</span>
-                        </div>
-
-
-                    </div>
-                </div>
-
-                <div className="home_img">
-                    <img src={home_image} alt="home-img" />
-                </div>
-
-
   useEffect(() => {
     AOS.init({
       duration: 1000, // Set the animation duration to 1200ms (1.2 seconds)
@@ -68,7 +20,7 @@ const Home = () => {
     <section className="home_cls">
       <div className="home-cntr">
         <div className="home_txt">
-          <p1 className="greetings">WELLCOME TO OUR BANKING WEB APPLICATION</p1>
+          <p1 className="greetings">Hello! Welcome to,</p1>
           <div
             data-aos="fade-right"
             data-aos-delay="0"
@@ -77,15 +29,15 @@ const Home = () => {
             <img src={banklogo} alt="" />
           </div>
           <p>
-            EMPOWERING YOUR FINANCIAL JOURNEY, WHERE TRUST MEETS TECHNOLOGY.
+            Let us help you invest in your future. It's our pleasure to work
+            with you.
           </p>
-          <h3>THE OFFICIAL BANKING WEB APPLICATION OF PRIMAL BANK</h3>
+          <h3>The official website of Primal Bank</h3>
 
           <div className="features">
             <div data-aos="fade-up-right" data-aos-delay="20" className="ft">
               <img src={easyP} alt="wallet_img" />
               <span>Easy Payments</span>
- main
             </div>
             <div data-aos="fade-up-right" data-aos-delay="20" className="ft">
               <img src={fstMny} alt="wallet_img" />
